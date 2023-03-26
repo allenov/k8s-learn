639250de1c7fc6c45fe980f414564290151916c5
Алленов Андрей Денисович <andrey.allenov@blacklight-it.ru>
2023-03-26 17:26:08 +0300

    Revert "Update k8s.sh"
    This reverts commit e9f7479ff5da955860cd5dd84fffe7d18c379bab.

--- k8s.sh
+++ k8s.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 curl -fsSL https://get.docker.com/ | sh
-systemctl start docker
-systemctl enable docker
+sudo systemctl start docker
+sudo systemctl enable docker
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x kubectl
-mv kubectl /usr/local/bin/
-usermod -aG docker $USER && newgrp docker
+sudo mv kubectl /usr/local/bin/
+sudo usermod -aG docker $USER && newgrp docker
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
 chmod +x ./kind
-mv ./kind /usr/local/bin/kind
+sudo mv ./kind /usr/local/bin/kind
 curl -O https://raw.githubusercontent.com/allenov/k8s-learn/main/kind-config.yaml
 kind create cluster --config kind-config.yaml
