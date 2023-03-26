6ce223fd61faca391b51413da8db787d28032884
allenoff <31571896+allenov@users.noreply.github.com>
2023-03-26 16:59:10 +0300

    Create k8s.sh

---
+++ k8s.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+curl -fsSL https://get.docker.com/ | sh
+sudo systemctl start docker
+sudo systemctl enabled docker
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+sudo usermod -aG docker $USER && newgrp docker
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+curl -O https://raw.githubusercontent.com/allenov/k8s-learn/main/kind-config.yaml
+kind create cluster --config kind-config.yaml
